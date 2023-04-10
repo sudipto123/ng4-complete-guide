2bb304ac00bdde7c75ba34a0a2ec48cea21dd720
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2023-04-10 18:54:14 +0530

    List Item added or updated

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Output } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
@@ -16,7 +16,25 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit() {
 
   }
-  onIngredientAdded(ingredient: Ingredient){
-    this.ingredients.push(ingredient);
+  onIngredientAdded(ingredient: Ingredient) {
+
+    if (!this.ingredients.find(e => e.name.toLowerCase() === ingredient.name.toLowerCase())) {
+      this.ingredients.push(ingredient);
+    }
+    else {
+      var oldIngredient: Ingredient = this.ingredients.find(e => e.name.toLowerCase() === ingredient.name.toLowerCase());
+      var oldAmount: number = +oldIngredient.amount;
+      var newAmount: number = +ingredient.amount;
+
+      var index = this.ingredients.indexOf(oldIngredient);
+
+      var updatedAmount: number = oldAmount + newAmount;
+      delete this.ingredients[index];
+
+      ingredient.amount = updatedAmount;
+      ingredient.name = ingredient.name;
+      
+      this.ingredients[index] = ingredient;
+    }
   }
 }
